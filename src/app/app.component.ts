44c3cf42c8380a00cacd805b5dd0722220b56370
iamPrashSri <prashantsrivastava.knowledge@gmail.com>
2020-07-16 16:57:07 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,44 @@ export class AppComponent  {
 
   viewMode = 'map';
 
-  courses = [
+  /* courses = [
     {id: 1, name: 'course1'},
     {id: 2, name: 'course2'},
     {id: 3, name: 'course3'},
-  ];
+  ]; */
+
+  courses = [];
+
+  canSave = true;
+
+  task = {
+    title: 'Review Applications',
+    assignee: {
+      name: 'John Smith'
+    }
+  }
+  
   clickMe(){
     
+  }
+
+  onAdd(){
+    this.courses.push({id: 4, name: 'course4'});
+  }
+
+  onRemove(courseId){
+    this.courses.splice(courseId, 1);
+  }
+
+  loadCourses(){
+    this.courses = [
+      {id: 1, name: 'course1'},
+      {id: 2, name: 'course2'},
+      {id: 3, name: 'course3'},
+    ];
+  }
+
+  trackCourse(index, course){
+    return course ? course.id : undefined;
   }
 }
