08d5efce262e692f816fac89962afbdb4d354488
iamPrashSri <prashantsrivastava.knowledge@gmail.com>
2020-07-16 23:23:46 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ImputFormatDirective } from './imput-format.directive';
+import { ZippyComponent } from './zippy/zippy.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ImputFormatDirective ],
+  declarations: [ AppComponent, HelloComponent, ImputFormatDirective, ZippyComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
