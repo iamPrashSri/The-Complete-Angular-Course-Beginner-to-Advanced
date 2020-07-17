bef88c93311740b513ef4056af3c83d15ba40255
iamPrashSri <prashantsrivastava.knowledge@gmail.com>
2020-07-17 15:09:51 +0530

    2 files modified

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent {
 
+  contactMethods = [
+    {id: 1, name:'Email'},
+    {id: 2, name:'Phone'},
+    {id: 3, name:'Whatsapp'}
+  ]
   log(x){
     console.log(x);
   }
