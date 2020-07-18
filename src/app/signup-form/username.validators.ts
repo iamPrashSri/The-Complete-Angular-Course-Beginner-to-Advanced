29d988f1fd56a068552a017283fe87f12e21f666
iamPrashSri <prashantsrivastava.knowledge@gmail.com>
2020-07-19 00:48:45 +0530

    1 file modified

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -1,13 +1,12 @@
 import { ValidationErrors, AbstractControl } from "@angular/forms";
 
 export class UsernameValidators {
+
   static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
     if ((control.value as string).indexOf(" ") >= 0) {
       return {
         cannotContainSpace: true
       };
-
-      return null;
       /* return {
         'minLength': {
           requiredLength : 3,
@@ -15,5 +14,21 @@ export class UsernameValidators {
         }
       } */
     }
+    return null;
   }
+
+    static shouldBeUnique(control: AbstractControl): ValidationErrors | null {
+      
+      // Asynchronous Operation. Need to di
+      setTimeout(() => {
+        if(control.value === 'mosh'){
+          return {
+            'shouldBeUnique' : true
+          };
+          return null;
+        }
+      }, 2000);
+
+      return null;
+    }
 }
