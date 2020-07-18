3c7f965de0cf053bc205934479160efbc62e12db
iamPrashSri <prashantsrivastava.knowledge@gmail.com>
2020-07-19 02:01:30 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
+import { NewcourseFormComponent } from './newcourse-form/newcourse-form.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
-  declarations: [ AppComponent, HelloComponent, ContactFormComponent, SignupFormComponent ],
+  declarations: [ AppComponent, HelloComponent, ContactFormComponent, SignupFormComponent, NewcourseFormComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/newcourse-form/newcourse-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'newcourse-form',
+  templateUrl: './newcourse-form.component.html',
+  styleUrls: ['./newcourse-form.component.css']
+})
+export class NewcourseFormComponent {
+
+}
