cbe7a149b59fac1dbc442cd90dbba8c78f7370c4
iamPrashSri <prashantsrivastava.knowledge@gmail.com>
2020-07-19 01:47:25 +0530

    2 files modified

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -11,14 +11,22 @@ import { UsernameValidators } from './username.validators';
 export class SignupFormComponent {
   form = new FormGroup({
     //userName: new FormControl('', Validators.required),
-    userName: new FormControl('', [Validators.required,
+    account: new FormGroup({
+      userName: new FormControl('', [Validators.required,
                                   Validators.minLength(3),
                                   UsernameValidators.cannotContainSpace], UsernameValidators.shouldBeUnique),
-    password: new FormControl('', Validators.required)
+      password: new FormControl('', Validators.required)})
   });
 
   // Getter function to have the value of form username in Component
   get userName(){
-    return this.form.get('userName');
+    return this.form.get('account.userName');
+  }
+
+  // Adding validator at runtime.
+  login(){
+    this.form.setErrors({
+      'invalidLogin' : true
+    });
   }
 }
