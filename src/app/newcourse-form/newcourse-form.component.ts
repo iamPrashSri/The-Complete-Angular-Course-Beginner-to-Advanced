9e5d8e1014f7737f71acbe9c1dc87123c4f23598
iamPrashSri <prashantsrivastava.knowledge@gmail.com>
2020-07-19 12:21:12 +0530

    1 file modified

--- src/app/newcourse-form/newcourse-form.component.ts
+++ src/app/newcourse-form/newcourse-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormArray, FormControl } from '@angular/forms';
+import { FormGroup, FormArray, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'newcourse-form',
@@ -8,8 +8,25 @@ import { FormGroup, FormArray, FormControl } from '@angular/forms';
 })
 export class NewcourseFormComponent {
   form = new FormGroup({
+    name: new FormControl('', Validators.required),
+    contact: new FormGroup({
+      email: new FormControl(),
+      phone: new FormControl()
+    }),
     topics: new FormArray([])
   });
+
+  // Working with formBuilder. Approach better than eariler one.
+  constructor (fb: FormBuilder){
+    fb.group({
+      name: ['', Validators.required], //Elements in this will be passed to FormControl object
+      contact: fb.group({
+        email: [],
+        phone: []
+      }),
+      topics: fb.array
+    });
+  }
 
   addTopic(topic: HTMLInputElement){
     // Typecasting in action
