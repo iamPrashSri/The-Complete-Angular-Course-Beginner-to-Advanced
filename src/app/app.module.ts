9b8d7886b880c7e4390e5b5e028c5a317aeb8ad1
Prashant Srivastava <prashantsrivastava.knowledge@gmail.com>
2020-07-20 22:43:53 +0530

    Imported new module @angular/common

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { NewcourseFormComponent } from './newcourse-form/newcourse-form.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
-  declarations: [ AppComponent, HelloComponent, ContactFormComponent, SignupFormComponent, NewcourseFormComponent ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule ],
+  declarations: [ AppComponent, HelloComponent, ContactFormComponent, SignupFormComponent, NewcourseFormComponent, PostsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
