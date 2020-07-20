0797a23d94eaf2966dd612900371768036828456
Prashant Srivastava <prashantsrivastava.knowledge@gmail.com>
2020-07-20 23:05:00 +0530

    Retrieving Data from Server

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-posts',
+  selector: 'posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  posts: any[];
+  constructor(http: HttpClient) { 
+    http.get('http://jsonplaceholder.typicode.com/posts').subscribe(response => {
+      //console.log(response);
+      this.posts = response;
+    });  
+  }
 
   ngOnInit(): void {
   }
